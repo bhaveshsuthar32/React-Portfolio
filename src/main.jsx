e52a39eab800c5c16251e09b8f58dfa917d02b26
unknown <bagrabhavesh426@gmail.com>
2024-06-14 12:24:58 +0530

    upload portfolio

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import "@fortawesome/fontawesome-free/css/all.min.css";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
